f80e737b55f9da5886b0c32c8d2ece0082ec95d3
Aditya Emani <prasaadem@gmail.com>
2017-03-30 19:14:55 -0500

    Google Places Integration- Search a place and locate it on map

--- RoadView/ViewController.swift
+++ RoadView/ViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import GooglePlaces
 
 enum TravelModes: Int {
     case driving
@@ -19,9 +19,9 @@ enum TravelModes: Int {
 class ViewController: UIViewController, CLLocationManagerDelegate, GMSMapViewDelegate {
     
     @IBOutlet weak var viewMap: GMSMapView!
+    @IBOutlet weak var destination: UITextField!
     
     @IBOutlet weak var lblInfo: UILabel!
-    
     
     var locationManager = CLLocationManager()
     
@@ -352,6 +352,44 @@ class ViewController: UIViewController, CLLocationManagerDelegate, GMSMapViewDel
             
             recreateRoute()
         }
+    }
+    
+    @IBAction func autocompleteClicked(_ sender: Any) {
+        let autocompleteController = GMSAutocompleteViewController()
+        autocompleteController.delegate = self
+        present(autocompleteController, animated: true, completion: nil)
+    }
+    
+}
+
+extension ViewController: GMSAutocompleteViewControllerDelegate {
+    
+    // Handle the user's selection.
+    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+        destination.text = place.name
+        print("Place: \(place)")
+        setupLocationMarker(place.coordinate)
+        viewMap.camera = GMSCameraPosition.camera(withTarget: place.coordinate, zoom: 10.0)
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+        // TODO: handle the error.
+        print("Error: ", error.localizedDescription)
+    }
+    
+    // User canceled the operation.
+    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // Turn the network activity indicator on and off again.
+    func didRequestAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
+    
+    func didUpdateAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
     }
     
 }
